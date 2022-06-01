c0a9ece4fab584b6fba31b677f875b352db54c6e
tutugodfrey <godfrey_tutu@yahoo.com>
2022-06-01 14:42:25 +0100

    Add code to update cloudfront

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,11 @@ if [ -z $AWS_PROFILE ]; then
   export AWS_PROFILE=uda-bot
 fi
 
+if [ -z $WorkflowID ]; then
+  WorkflowID=udapeople-kk1j287dhjppmz437
+fi
+
 aws cloudformation deploy \
   --template-file .circleci/files/cloudfront.yml \
   --stack-name InitialStack\
-  --parameter-overrides WorkflowID=udapeople-kk1j287dhjppmz437
-  
+  --parameter-overrides WorkflowID=${WorkflowID}
